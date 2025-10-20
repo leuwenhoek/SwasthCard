2938f375e4f5c92a7796d4d35b80d50869397501
leuwenhoek <ninjabeastyy24@gmail.com>
2025-10-20 21:42:52 +0530

    login details added

--- web/app.py
+++ web/app.py
@@ -2,22 +2,33 @@ from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
+# send user to home page that he/she is a doctor or patient
 @app.route("/",methods=["GET","POST"])
 def home():
     role = request.form.get('role')
-    print(role)
     if role == "doctor":
         return redirect("/doctor_login")
     elif role == "patient":
         return redirect("/patient_login")
     return render_template("Home.html")
 
+#login page for doctor and patient 
 @app.route("/doctor_login",methods=["GET","POST"])
 def doctor():
+    doctor_code = request.form.get('code')
+    password = request.form.get('pass')
+    if doctor_code == "1234" and password == "1234":  # login pass is "doctor code : 1234" and "password : 1234"
+        return redirect("/")
     return render_template("Doctor_login.html")
 
 @app.route("/patient_login",methods=["GET","POST"])
 def patient():
+    RFID_num = request.form.get('num')
+    password = request.form.get('pass')
+    print(RFID_num)
+    print(password)
+    if RFID_num == "2025" and password == "Ayush":  # login pass is "RFID Number : 2025" and "password : Ayush"
+        return redirect("/")
     return render_template("Patient_login.html")
 
 @app.route("/profile")
